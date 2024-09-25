6f293c22ecc33f056e79d80b6705eaf34c9353b9
xxxworldd <mmmmmzmmmmmz66@gmail.com>
2024-09-25 10:24:27 +0300

    hotfix

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ def get_math_text(a, b, c):
     x1=((b*-1)+(d**0.5))/(2*a)
     x2=((b*-1)-(d**0.5))/(2*a)
     return [x1,x2]
-a=input('Введите коэффицент a')
-b=input('Введите коэффицент b')
-c=input('Введите коэффицент c')
+a=input('Введите любой a')
+b=input('Введите любой b')
+c=input('Введите любой c')
 d=get_math_text(a,b,c)
 print(d)
 def summ(s1, s2):
