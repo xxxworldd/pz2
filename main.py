12a7c394498180dbb4464bdf702fdf5e23ca395e
xxxworldd <mmmmmzmmmmmz66@gmail.com>
2024-09-25 10:10:48 +0300

    create front

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ def get_math_text(a, b, c):
     x1=((b*-1)+(d**0.5))/(2*a)
     x2=((b*-1)-(d**0.5))/(2*a)
     return [x1,x2]
+a=input('Введите коэффицент a')
+b=input('Введите коэффицент b')
+c=input('Введите коэффицент c')
+d=get_math_text(a,b,c)
+print(d)
